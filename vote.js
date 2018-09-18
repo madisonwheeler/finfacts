d4d9cc58e6cfd4e4b7db8a8ceed766919d1208f3
maryamhed <maryamh95@gmail.com>
2018-09-18 14:59:49 -0500

    clicking a choice changes all of them

---
+++ vote.js
@@ -0,0 +1,37 @@
+var b1 = document.getElementById("b1");
+var b2 = document.getElementById("b2");
+var b3 = document.getElementById("b3");
+var buttons = document.querySelectorAll(".unrevealed");
+var labels = document.querySelectorAll("label");
+var next = document.getElementById("next");
+console.log(labels);
+
+b1.addEventListener("click", function(){
+  next.removeAttribute("hidden");
+  buttons.forEach(function(item) {
+    item.classList.add("revealed");
+    item.classList.remove("unrevealed");
+  });
+  labels.forEach(function(item) {
+    item.removeAttribute("hidden");
+  });})
+
+b2.addEventListener("click", function(){
+  next.removeAttribute("hidden");
+  buttons.forEach(function(item) {
+    item.classList.add("revealed");
+    item.classList.remove("unrevealed");
+  });
+  labels.forEach(function(item) {
+    item.removeAttribute("hidden");
+  });})
+
+b3.addEventListener("click", function(){
+  next.removeAttribute("hidden");
+  buttons.forEach(function(item) {
+    item.classList.add("revealed");
+    item.classList.remove("unrevealed");
+  });
+  labels.forEach(function(item) {
+    item.removeAttribute("hidden");
+  });})
