9ca759d048767df3390bf13a16fcadfd14c6ac6c
maryamhed <maryamh95@gmail.com>
2018-09-19 11:30:47 -0500

    flex bars flex

--- vote.js
+++ vote.js
@@ -6,7 +6,7 @@ var labels = document.querySelectorAll("label");
 var next = document.getElementById("next");
 console.log(labels);
 
-function revealAnswers(){
+function revealAnswers(num){
   next.removeAttribute("hidden");
   buttons.forEach(function(item) {
     item.classList.add("revealed");
@@ -15,16 +15,25 @@ function revealAnswers(){
   labels.forEach(function(item) {
     item.removeAttribute("hidden");
   });
+  var button = document.getElementById("b" + num);
+  var symbol;
+  if (button.classList.contains("truth-button")) {
+    symbol = document.getElementById("w" + num);
+  }
+  else {
+    symbol = document.getElementById("r" + num);
+  }
+    symbol.removeAttribute("hidden");
 };
 
 b1.addEventListener("click", function(){
   b1.classList.add("selected");
-  revealAnswers();})
+  revealAnswers(1);})
 
 b2.addEventListener("click", function(){
   b2.classList.add("selected");
-  revealAnswers();})
+  revealAnswers(2);})
 
 b3.addEventListener("click", function(){
   b3.classList.add("selected");
-  revealAnswers();})
+  revealAnswers(3);})
