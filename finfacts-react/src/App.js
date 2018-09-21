982645046ad51bba6e4fde47d63ae99006eea94c
Madison Wheeler <madisonwheeler@MWHEELER-MBP-NY.local>
2018-09-21 09:52:06 -0500

    increased percentage font

--- finfacts-react/src/App.js
+++ finfacts-react/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {NavBar} from './NavBar';
 import {Submit} from './Submit';
 import {Vote} from './Vote';
-import logo from './logo.png';
 //import './App.css';
 
 class App extends Component {
@@ -29,7 +28,7 @@ class App extends Component {
     return (
       <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <img src={logo} width="50" height="50" className="logo" marginRight="10px"></img>
+        <a className="navbar-brand">FinFacts</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -37,7 +36,7 @@ class App extends Component {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
           <button className="btn nav-button" onClick={this.toVote}>Vote</button>
-          <button className="btn nav-button" onClick={this.toSubmit}>Submit</button>
+          <button className="btn nav-button" onClick={this.toSubmit} >Submit</button>
 
 
           </ul>
