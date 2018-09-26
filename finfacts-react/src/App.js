b5cb6a63f7ece5738405471de45752cc02d06308
maryamhed <maryamh95@gmail.com>
2018-09-26 12:53:18 -0400

    added logo back

--- finfacts-react/src/App.js
+++ finfacts-react/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {NavBar} from './NavBar';
 import {Submit} from './Submit';
 import {Vote} from './Vote';
-//import './App.css';
+import logo from './logo.png';
 
 class App extends Component {
   constructor(){
@@ -28,7 +28,7 @@ class App extends Component {
     return (
       <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand">FinFacts</a>
+        <img src={logo} width="50" height="50" className="logo" marginRight="10px"></img>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
